409018464375e1f2600bef66597d9a112b957496
Alem Burzic <alem.burzic1@gmail.com>
2025-11-06 17:33:26 +0100

    Implement registration and login with hashed passwords
    - Added RegisterAgentDTO and LoginAgentDTO
    - Added IAgentRepository interface for login and registration
    - Implemented hashing for passwords in repository
    - Added AgentController with endpoints for register and login

---
+++ Backend/ClientHub/ClientHub/Interfaces/IAgentRepository.cs
@@ -0,0 +1,13 @@
+﻿using ClientHub.DTOs;
+using ClientHub.Models;
+
+namespace ClientHub.Interfaces
+{
+    public interface IAgentRepository
+    {
+
+        public Task<Agent> RegisterAgent(RegisterAgentDTO registerAgentDTO, CancellationToken ct);
+
+        public Task<Agent?> LoginAgent(LoginAgentDTO loginAgentDTO, CancellationToken ct);
+    }
+}
